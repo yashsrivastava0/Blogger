112bc91f98b76a94290b7206b0e88ba6d587057a
yashsrivastava0 <161153410+yashsrivastava0@users.noreply.github.com>
2024-12-31 02:15:49 +0530

    Initial commit

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function NotFound() {
+  return (
+    <div className="flex flex-col h-screen items-center justify-center">
+      <h1 className="text-2xl font-semibold">404</h1>
+      <span className="text-xl text-gray-800">Page Not Found</span>
+    </div>
+  );
+}
+
+export default NotFound;
